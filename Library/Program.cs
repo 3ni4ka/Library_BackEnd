63a1d6807bb088d75053a3f9feaff8ae21f3d443
3ni4ka <120841964+3ni4ka@users.noreply.github.com>
2022-12-18 02:29:28 +0300

    Добавлено логирование

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,6 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Library;
+using Microsoft.Extensions.FileProviders;
+using NLog.Extensions.Logging;
 
-app.MapGet("/", () => "Hello World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.WebHost.UseUrls();
 
-app.Run();
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                     var appPath = AppDomain.CurrentDomain.BaseDirectory;
+                     var configPath = Path.Combine(appPath, @"Configs");
+                    if (Directory.Exists(configPath))
+                        configBuilder.AddJsonFile(new PhysicalFileProvider(configPath), @"config.json", false, false);
+                    else
+                        configBuilder.AddJsonFile("config.json");
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(configLogging =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                    configLogging.AddNLog("Nlog.config");
+                });
+    
+}
---
+++ Library/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace Library
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();   // добавляем поддержку статических файлов
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!!!!");
+            });
+        }
+    }
+}
